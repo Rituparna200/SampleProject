33b7309c95d1d0585858334f53ad3738a96cea09
Rituparna200 <142205900+Rituparna200@users.noreply.github.com>
2023-08-15 20:36:01 +0530

    subtraction

--- Sumnumbe1.java
+++ Sumnumbe1.java
@@ -3,7 +3,7 @@ public class Sumnumbe1
 public static void main(String args[])   
 {  
 int n1 = 234, n2 = 121, sum;  
-sum = n1 - n2;  
-System.out.println("The sum of numbers is: "+sum);  
+sub = n1 - n2;  
+System.out.println("The sub of numbers is: "+sub);  
 }  
 }
