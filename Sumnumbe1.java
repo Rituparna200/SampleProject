da73ff636caa69d93ff02e304ae40fd29db7b0f5
Rituparna200 <samanta002ritu@gmail.com>
2023-08-15 20:22:04 +0530

    First command

---
+++ Sumnumbe1.java
@@ -0,0 +1,9 @@
+public class Sumnumbe1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 650, n2 = 365, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}
